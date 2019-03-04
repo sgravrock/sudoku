2f1bf2d6aa116b9c0c6988e8f492d13951285f15
Steve Gravrock <sdg@panix.com>
2019-03-03 18:00:32 -0800

    Improved overwrite-related interactions
    * Don't overwrite existing non-pencil marks
    * Remove an existing pencil mark rather than doubling it up

--- src/ToolApplier.test.ts
+++ src/ToolApplier.test.ts
@@ -17,11 +17,17 @@ describe('ToolApplier', () => {
 				expect(result.cell(0, 0).entry).toEqual({n: 1, pencil: false});
 			});
 
-			it('does not replace given values', () => {
-				const puzzle = makePuzzle(2);
+			it('erases non-pencil entries that have the same number', () => {
+				const puzzle = makePuzzle(null).setCell(0, 0, {n: 1, pencil: false});
 				const result = applyTool(normal(1), 0, 0, puzzle);
-				expect(result.cell(0, 0).entry).toEqual({n: 3, pencil: false});
-			})
+				expect(result.cell(0, 0).entry).toBeNull();
+			});
+
+			it('does not replace non-pencil entries that have other numbers', () => {
+				const puzzle = makePuzzle(null).setCell(0, 0, {n: 2, pencil: false});
+				const result = applyTool(normal(1), 0, 0, puzzle);
+				expect(result.cell(0, 0).entry).toEqual({n: 2, pencil: false});
+			});
 		});
 
 		describe('Pencil', () => {
@@ -35,6 +41,29 @@ describe('ToolApplier', () => {
 				const puzzle = makePuzzle(null).setCell(0, 0, {ns: [2], pencil: true});
 				const result = applyTool(pencil(1), 0, 0, puzzle);
 				expect(result.cell(0, 0).entry).toEqual({ns: [2, 1], pencil: true});
+			});
+
+			describe('When the cell already has this pencil mark', () => {
+				describe('And no others', () => {
+					it('clears the cell', () => {
+						const puzzle = makePuzzle(null)
+							.setCell(0, 0, {ns: [1], pencil: true});
+						const result = applyTool(pencil(1), 0, 0, puzzle);
+						expect(result.cell(0, 0).entry).toBeNull();
+					});
+				});
+
+				describe('And some others', () => {
+					it('removes the specified number', () => {
+						const puzzle = makePuzzle(null)
+							.setCell(0, 0, {ns: [1, 2, 3], pencil: true});
+						const result = applyTool(pencil(2), 0, 0, puzzle);
+						expect(result.cell(0, 0).entry).toEqual({
+							ns: [1, 3],
+							pencil: true
+						});
+					});
+				});
 			});
 
 			it('does not replace normal numbers', () => {
--- src/ToolApplier.ts
+++ src/ToolApplier.ts
@@ -14,12 +14,27 @@ export function applyTool(tool: Tool, x: number, y: number, puzzle: Puzzle): Puz
 			if (tool.pencil) {
 				return applyPencil(puzzle, x, y, tool.n);
 			} else {
-				return puzzle.setCell(x, y, {n: tool.n, pencil: false});
+				return applyNormal(puzzle, x, y, tool.n);
 			}
 	}
 
 	return puzzle;
 }
+
+function applyNormal(puzzle: Puzzle, x: number, y: number, n: number): Puzzle {
+	const entry = puzzle.cell(x, y).entry;
+
+	if (entry && !entry.pencil) {
+		if (entry.n === n) {
+			return puzzle.setCell(x, y, null);
+		} else {
+			return puzzle;
+		}
+	}
+
+	return puzzle.setCell(x, y, {n, pencil: false});
+}
+
 
 function applyPencil(puzzle: Puzzle, x: number, y: number, n: number): Puzzle {
 	let entry = puzzle.cell(x, y).entry;
@@ -28,10 +43,14 @@ function applyPencil(puzzle: Puzzle, x: number, y: number, n: number): Puzzle {
 		return puzzle;
 	}
 
-	if (entry) {
-		entry = {...entry, ns: [...entry.ns, n]};
-	} else {
+	if (!entry) {
 		entry = {pencil: true, ns: [n]};
+	} else if (!entry.ns.includes(n)) {
+		entry = {...entry, ns: [...entry.ns, n]};
+	} else if (entry.ns.length === 1) {
+		entry = null;
+	} else {
+		entry = {...entry, ns: entry.ns.filter(x => x !== n)};
 	}
 
 	return puzzle.setCell(x, y, entry);
